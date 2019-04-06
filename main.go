bd794a07290a62c6d186f7c8f27d99b18f46af81
Brandon <brandon2255p@gmail.com>
2019-04-06 08:21:48 +0200

    Added mongo eventstore

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 
+	"github.com/google/uuid"
 	"github.com/looplab/eventhorizon/commandhandler/bus"
 	eventbus "github.com/looplab/eventhorizon/eventbus/local"
 	eventstore "github.com/looplab/eventhorizon/eventstore/mongodb"
@@ -20,20 +21,35 @@ const (
 )
 
 // server is used to implement helloworld.GreeterServer.
-type server struct{}
+type server struct {
+	commandBus *bus.CommandHandler
+}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) Hello(ctx context.Context, in *pb.Request) (*pb.Response, error) {
 	log.Printf("Received: %v", in.Name)
+
+	id, _ := uuid.Parse("25eb3db9-5833-11e9-b3ed-ac35ee1f3de8")
+	err := s.commandBus.HandleCommand(ctx, &domain.ChangeRestaurantNameCommand{
+		ID:   id,
+		Name: in.Name,
+	})
+	if err != nil {
+		return &pb.Response{Msg: "Hello we had an error"}, nil
+	}
 	return &pb.Response{Msg: "Hello " + in.Name}, nil
+
 }
 
 func main() {
 	mongoURL := os.Getenv("MONGO_HOST")
+	if mongoURL == "" {
+		mongoURL = "localhost"
+	}
 
 	es, err := eventstore.NewEventStore(mongoURL, "restaurant")
 	if err != nil {
-		panic(fmt.Sprintf("Unable to establish eventstore - %s", err))
+		panic(fmt.Sprintf("Unable to establish eventstore - %s", err.Error()))
 	}
 
 	eb := eventbus.NewEventBus(nil)
@@ -44,12 +60,13 @@ func main() {
 	// }
 	ch := bus.NewCommandHandler()
 	domain.Setup(es, eb, ch)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterGreeterServer(s, &server{commandBus: ch})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
